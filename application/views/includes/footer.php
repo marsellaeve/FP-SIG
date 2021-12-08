eadc01859d3424f2d7bc7b2c9e2e79d25ee9270a
marsellaeve <marsella.eve@gmail.com>
2021-12-08 15:49:37 +0700

    fp-sig

---
+++ application/views/includes/footer.php
@@ -0,0 +1,29 @@
+<style>
+    #footer {
+        margin-top:10px;
+        display:block;    
+        text-align: center;  
+        color: #fff;
+        font-size: 1.2em;
+        padding: 20px;
+        line-height: 16px;
+        clear: both;
+        min-height:100px;
+        position:relative;
+        width:100%;
+        z-index:900;
+  }
+  
+#footer a { text-decoration: none; color: #fff;}
+#footer a:hover { color: #fff; }
+</style>
+	<div id="footer" class="orange">
+		<div class="left">
+		<strong>Evelyn Tjitrodjojo 05111840000099</strong>
+		</div>			
+		<div class="right" style="position:fixed;bottom:0;right:0;">
+			<a href="#" class="btn btn-flat btn-large" title="Top" alt="Top"><i class="large material-icons">expand_less</i></a>
+		</div>
+	</div>
+</body>
+</html>
---
+++ application/views/includes/sidebar.php
@@ -0,0 +1,14 @@
+<div class="card-image">
+    <div class="background">
+        <img src="<?php echo base_url('images/slider/rumah.jpg');?>" class="responsive-img z-depth-3">
+    </div>
+</div>
+<ul class="nav sidenav" id="slide-out">
+    <li><a href="#" style="pointer-events: none;"><img src="<?php echo base_url('images/profile/eveprofile.jpeg')?>" class="circle" alt="Profile" style="width:50px;height:50px;"></a></li>
+    <li>
+            <a href="#" style="pointer-events: none;"><span class="white-text name">Evelyn Tjitrodjojo</span></a>
+            <a href="#" style="pointer-events: none;"><span class="white-text email">marsella.eve@gmail.com</span></a>
+    </li>
+    <li><a class="waves-effect white-text" href="<?php echo site_url('peta/index')?>"><i class="material-icons white-text">burst_mode</i>Peta</a></li>
+    <li><a class="waves-effect white-text" href="<?php echo site_url('pembanding/index')?>"><i class="material-icons white-text">assignment</i>List</a></li>
+</ul>       
